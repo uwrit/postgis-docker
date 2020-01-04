7237d08a89b08ce9712e7cabf35712533404ed76
ndobb <ndobb@uw.edu>
2020-01-04 15:52:04 -0800

    Removed pooling

--- src/server/flaskr/app/modules/postgis.py
+++ src/server/flaskr/app/modules/postgis.py
@@ -1,10 +1,5 @@
 import psycopg2
-import json
 import os
-import sys
-import time
-
-from psycopg2 import pool
 
 class PostgisConnector:
 
@@ -13,42 +8,34 @@ class PostgisConnector:
         self.user = os.environ['POSTGRES_USER']
         self.pwd = os.environ['POSTGRES_PASSWORD']
         self.db = os.environ['POSTGRES_DB']
-        
-    def __get_pool(self):
-        self.pool = psycopg2.pool.SimpleConnectionPool(1, 20,
-                                host=self.host,
+
+    def __get_connection(self):
+        conn = psycopg2.connect(host=self.host,
                                 database=self.db,
                                 user=self.user,
                                 password=self.pwd)
+        conn.set_session(autocommit=True)
+        return conn
 
-    def __get_connection(self):
-        if not self.pool:
-            self.__get_pool()
-        return self.pool.getconn()
+    def get_lat_long(self, var_address):
 
-    def __close_connection(self, conn):
-        self.pool.putconn(conn)
-    
-    def get_lat_long(self, addr):
-
-        conn = self.__get_connection()
-        with conn.cursor() as cursor:
-            cursor.execute("""
-                SELECT 
-                    g.rating
-                  , ST_AsText(ST_SnapToGrid(g.geomout,0.00001)) As wktlonlat
-                  , (addy).address As stno
-                  , (addy).streetname As street
-                  , (addy).streettypeabbrev As styp
-                  , (addy).location As city
-                  , (addy).stateabbrev As st
-                  , (addy).zip
-                FROM geocode(%(addr)s) As g;
-            """, {
-                'addr': addr
-            })
-            result = cursor.fetchone()
-        self.__close_connection(conn)
+        with self.__get_connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("""
+                    SELECT 
+                      g.rating
+                    , ST_AsText(ST_SnapToGrid(g.geomout,0.00001)) As wktlonlat
+                    , (addy).address As stno
+                    , (addy).streetname As street
+                    , (addy).streettypeabbrev As styp
+                    , (addy).location As city
+                    , (addy).stateabbrev As st
+                    , (addy).zip
+                    FROM geocode(%(var_address)s) As g;
+                """, {
+                    'var_address': var_address
+                })
+                result = cursor.fetchone()
 
         if result is None:
             return None

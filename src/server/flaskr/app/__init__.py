a9840e783aed489fb82c8c0bdea14fcfd3cc7fd0
ndobb <ndobb@uw.edu>
2020-01-03 22:42:57 -0800

    Testing flask

--- src/server/flaskr/app/__init__.py
+++ src/server/flaskr/app/__init__.py
@@ -4,29 +4,35 @@ import uuid
 
 from flask import Flask, Request, request, jsonify
 from .modules.response import ok, bad_request, forbidden, not_found, server_error
-from .modules.manager import Manager
+from .modules.postgis import PostgisConnector
 
 app = Flask(__name__)
-mgr = Manager()
+postgis = PostgisConnector()
 
 #########################################
 # Routes
 #########################################
-@app.route('/user', methods=['GET'])
-def is_user():
+@app.route('/latlong', methods=['GET'])
+def latlong():
     try:
-        email = request.args.get('email')
-        entry_code = request.args.get('entry_code')
+        for arg in request.args.items():
+            sys.stdout.write(f'{arg[0]}:{arg[1]}')
+        address = request.args.get('address')
         
-        if not email or not entry_code:
+        if not address:
             return bad_request()
 
-        user = mgr.get_user(email, entry_code)
-        if user:
-            return ok({ 'user' : user })
+        data = postgis.get_lat_long(address)
+        if data:
+            data = jsonify({ 'data': data })
+            return ok(data)
 
         return not_found()
 
     except Exception as ex:
         sys.stderr.write(f'Error: {ex}\n')
         return server_error()    
+
+@app.route('/helloworld', methods=['GET'])
+def hello():
+    return ok(jsonify({ 'data': 'Hello World!' }))
--- src/server/flaskr/app/modules/postgis.py
+++ src/server/flaskr/app/modules/postgis.py
@@ -1,40 +1,46 @@
 import psycopg2
 import json
 import os
+import sys
 
 from datetime import datetime
 
 class PostgisConnector:
 
     def __init__(self):
-        token, url = self.__load_config()
-        self.url = url
-        self.token = token
+        self.host = 'postgis-docker_db_1'
+        self.user = os.environ['POSTGRES_USER']
+        self.pwd = os.environ['POSTGRES_PASSWORD']
+        self.db = os.environ['POSTGRES_DB']
+        self.conn = psycopg2.connect(host=self.host,
+                                     database=self.db,
+                                     user=self.user,
+                                     password=self.pwd)
+        self.conn.set_session(autocommit=True)
 
-    def __load_config(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
+    def get_lat_long(self, var_address):
 
-        # Assume config is in the same directory.
-        with open(f'{dir_path}{os.path.sep}config.json') as f:
-            config = json.load(f)
-            return config['redcap']['token'], config['redcap']['url']
+        with self.conn.cursor() as cursor:
+            cursor.execute("""
+                SELECT 
+                    g.rating
+                  , ST_AsText(ST_SnapToGrid(g.geomout,0.00001)) As wktlonlat
+                  , (addy).address As stno
+                  , (addy).streetname As street
+                  , (addy).streettypeabbrev As styp
+                  , (addy).location As city
+                  , (addy).stateabbrev As st
+                  , (addy).zip
+                FROM geocode(%(var_address)s) As g;
+            """, {
+                'var_address': var_address
+            })
+            result = cursor.fetchone()
 
-    def __call_api(self, data):
-
-        data['token'] = self.token
-        data['format'] = 'json'
-        resp = requests.post(self.url, data=data)
-
-        if resp.status_code != 200:
+        if result is None:
             return None
 
-        return json.loads(resp.text)
-
-    def get_data(self):
-
-        data = {
-            'content': 'record',
-            'type': 'flat'
-        }
-        return self.__call_api(data)
+        sys.stdout.write(result)
+        wktlonlat, = result
+        return json.loads(wktlonlat)
     
